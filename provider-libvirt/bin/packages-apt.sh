1d4b7f2b727320496c60f91a5c0c2c9d52f4223e
ecla_LE <ecla_LE@proton.me>
2023-10-04 22:28:33 +0200

    removed ppa-purge from basic packages provisioning

--- provider-libvirt/bin/packages-apt.sh
+++ provider-libvirt/bin/packages-apt.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y build-essential libssl-dev libffi-dev apt-transport-https curl python3 python3-pip
-sudo apt install -y software-properties-common ppa-purge
+sudo apt install -y software-properties-common
 
 exit 0;
