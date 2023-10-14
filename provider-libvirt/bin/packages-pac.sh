461a50b4301646f85b082ed4e7b85803e67824f5
ecla_LE <ecla_LE@proton.me>
2023-10-14 14:55:44 +0200

    added inetutils to arch vm setup script

--- provider-libvirt/bin/packages-pac.sh
+++ provider-libvirt/bin/packages-pac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm base-devel
+sudo pacman -S --noconfirm base-devel inetutils
 sudo pacman -S --noconfirm python python-pip
 sudo pacman -S --noconfirm reflector
 
